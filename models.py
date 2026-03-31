c442ac97cd30671a6e28191df04274bc0534371a
Isabella Morgan <isabellataylormorgan@gmail.com>
2026-03-30 22:20:33 -0500

    Refactor User and Student classes with comments
    Added comments for clarity and structure in models.py.

--- models.py
+++ models.py
@@ -3,9 +3,13 @@ class User:
         self.username = username
         self.role = role
 
-
+#student class
+#inherits from user
 class Student(User):
     def __init__(self, username, transcript, degree_plan):
         super().__init__(username, "student")
         self.transcript = transcript
         self.degree_plan = degree_plan
+
+#possibly will need to make a class for faculty
+# but for testing purposes so far its not needed
