06f6cb6cc4101a9c95d683f839a4b7ddbe7a9d53
Isabella Morgan <isabellataylormorgan@gmail.com>
2026-03-30 22:19:39 -0500

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -1,7 +1,14 @@
+#recommendation engine
+#this will need updating to accomodate UTRGV CYBI degree road map
+#for testing purpose and basic implementation this works
+#this inherits from Course classs and the student data
 class RecommendationEngine:
     def generate(self, completed, degree_plan, prerequisites):
         recommended = []
-
+        #essentially if course in the degree plan
+        #and its not completed then first look for the
+        #prerequisites, if there's no prerequisite
+        #then appened to course and return it as recommended
         for course in degree_plan:
             if course not in completed:
                 prereqs = prerequisites.get(course, [])
@@ -9,3 +16,4 @@ class RecommendationEngine:
                     recommended.append(course)
 
         return recommended
+#this is used in the recommendation report
